e5ed12f34aa6385893b3a0fdf8f289d553271e09
Riley W <98372125+riley-wood@users.noreply.github.com>
2023-05-18 13:42:19 -0600

    Minor functional changes

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+# calculator.py
+
 def add(x, y):
     return x + y
 
@@ -5,7 +7,7 @@ def subtract(x, y):
     return x - y
 
 def multiply(x, y):
-    return x * y
+    return x - y
 
 def divide(x, y):
     if y != 0:
