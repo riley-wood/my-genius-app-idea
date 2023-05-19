b88279e64e195b8806af76526e1d4e0d6ef2715e
Riley W <98372125+riley-wood@users.noreply.github.com>
2023-05-19 08:58:36 -0600

    Line spacing

--- calculator.py
+++ calculator.py
@@ -3,11 +3,14 @@
 def add(x, y):
     return x + y
 
+
 def subtract(x, y):
     return x - y
 
+
 def multiply(x, y):
     return x - y
+
 
 def divide(x, y):
     if y != 0:
@@ -15,8 +18,10 @@ def divide(x, y):
     else:
         print("Error: Division by zero is not allowed")
 
+        
 def power(x, y):
     return x ** y
+
 
 print(add(5, 3))
 print(subtract(5, 3))
